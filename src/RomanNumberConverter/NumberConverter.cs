26cd86068d7fe545a46830526c8a86f21c6d7ecc
rajthapa2 <raja_thapa34@yahoo.com>
2013-08-10 11:15:23 +0100

    <Raja>Added the nunit test in the solution

---
+++ src/RomanNumberConverter/NumberConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace RomanNumberConverter
+{
+    public class NumberConverter
+    {
+        public string ConvertNumberToRomanNumber(int number)
+        {
+            if(number ==1 )
+            {
+                return "I";
+            }
+            return number == 2 ? "II" : "III";
+        }
+    }
+}
---
+++ src/RomanNumberTests/RomanNumberConverterTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using RomanNumberConverter;
+
+namespace RomanNumberTests
+{
+    [TestFixture] 
+    public class RomanNumberConverterTests
+    {
+        private NumberConverter _numberConverter;
+      
+        [SetUp]
+        public void  SetUp()
+        {
+         _numberConverter = new NumberConverter();
+        }
+     
+      [Test]  
+      public void ShouldReturnRomanNumberIWhen1IsPassed()
+      {
+          Assert.That(_numberConverter.ConvertNumberToRomanNumber(1), Is.EqualTo("I"));
+      }
+        [Test]
+        public void ShouldReturnRomanNumberIIWhen2IsPassed()
+        {
+            Assert.That(_numberConverter.ConvertNumberToRomanNumber(2), Is.EqualTo("II"));
+        }
+
+        [Test]
+        public void ShouldReturnRomanNumberIIIWhen3IsPassed()
+        {
+            Assert.That(_numberConverter.ConvertNumberToRomanNumber(3), Is.EqualTo("III"));
+        }
+    }
+}
