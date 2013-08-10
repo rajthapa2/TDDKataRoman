1bd7f0f18c0748a0f862661def9418bb9de17c85
rajthapa2 <raja_thapa34@yahoo.com>
2013-08-10 12:09:42 +0100

    <raj>Added a method map

--- src/RomanNumberConverter/NumberConverter.cs
+++ src/RomanNumberConverter/NumberConverter.cs
@@ -1,14 +1,25 @@
-﻿namespace RomanNumberConverter
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanNumberConverter
 {
     public class NumberConverter
     {
         public string ConvertNumberToRomanNumber(int number)
         {
-            if(number ==1 )
-            {
-                return "I";
-            }
-            return number == 2 ? "II" : "III";
+            return Map(number);
+        }
+
+        private string Map(int number)
+        {
+            var mapper = new Dictionary<int, string>();
+            mapper.Add(1, "I");
+            mapper.Add(2, "II");
+            mapper.Add(3, "III");
+            mapper.Add(4, "IV");
+            mapper.Add(5, "V");
+
+            return (from x in mapper where x.Key == number select x.Value).First();
         }
     }
 }
--- src/RomanNumberTests/RomanNumberConverterTests.cs
+++ src/RomanNumberTests/RomanNumberConverterTests.cs
@@ -14,21 +14,30 @@ namespace RomanNumberTests
          _numberConverter = new NumberConverter();
         }
      
-      [Test]  
-      public void ShouldReturnRomanNumberIWhen1IsPassed()
+      [TestCase(1,"I")]  
+      [TestCase(2,"II")]  
+      [TestCase(3,"III")]  
+      [TestCase(4,"IV")]  
+      public void ShouldMapNumberToRomanNumber(int number, string romanNumber)
       {
-          Assert.That(_numberConverter.ConvertNumberToRomanNumber(1), Is.EqualTo("I"));
+          Assert.That(_numberConverter.ConvertNumberToRomanNumber(number), Is.EqualTo(romanNumber));
       }
-        [Test]
-        public void ShouldReturnRomanNumberIIWhen2IsPassed()
-        {
-            Assert.That(_numberConverter.ConvertNumberToRomanNumber(2), Is.EqualTo("II"));
-        }
-
-        [Test]
-        public void ShouldReturnRomanNumberIIIWhen3IsPassed()
-        {
-            Assert.That(_numberConverter.ConvertNumberToRomanNumber(3), Is.EqualTo("III"));
-        }
+//        [Test]
+//        public void ShouldReturnRomanNumberIIWhen2IsPassed()
+//        {
+//            Assert.That(_numberConverter.ConvertNumberToRomanNumber(2), Is.EqualTo("II"));
+//        }
+//
+//        [Test]
+//        public void ShouldReturnRomanNumberIIIWhen3IsPassed()
+//        {
+//            Assert.That(_numberConverter.ConvertNumberToRomanNumber(3), Is.EqualTo("III"));
+//        }
+//
+//        [Test]
+//        public void ShouldReturnEmptyStringWhenInvalidNumberIsPassed()
+//        {
+//            Assert.That(_numberConverter.ConvertNumberToRomanNumber(4),Is.EqualTo(""));
+//        }
     }
 }
